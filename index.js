2ee3a144f3c48480a16805951e4bf48a053174fd
Vedant794 <vedantkumbhare721@gmail.com>
2025-03-25 11:56:40 +0530

    Hello World route initialize

---
+++ index.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+const express = require("express");
+
+const app = express();
+
+app.get("/", async (req, res) => {
+  res.send("Hello World");
+});
+
+const PORT = process.env.PORT | 80;
+
+app.listen(PORT, () => console.log(`Server Started at Port:${PORT}`));
