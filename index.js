70bff209365cf28d2c5bc9a5db641eef9a3b1e63
shrey209 <144334177+shrey209@users.noreply.github.com>
2025-04-09 15:58:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", async (req, res) => {
-  res.send("Hello World test1");
+  res.send("Hello this is cicd with jenkins and argocd");
 });
 
 const PORT = process.env.PORT | 80;
