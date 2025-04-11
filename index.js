b507560589c4e88f18ef7b31b1da8a2c83616ddc
Vedant794 <132437646+Vedant794@users.noreply.github.com>
2025-04-11 11:33:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", async (req, res) => {
-  res.send("Umesh Bhau ka Project");
+  res.send("Umesh Bhau lavda hai ka Project");
 });
 
 const PORT = process.env.PORT | 80;
