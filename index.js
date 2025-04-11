1f040fcfa9359bf2d8d1550fe41970bc4b81aad9
Vedant794 <132437646+Vedant794@users.noreply.github.com>
2025-04-11 11:42:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", async (req, res) => {
-  res.send("bhushan Bhau lavda hai ka Project");
+  res.send("Final Project With Jenkins and github By our Team : Vedant , Shreyash, Bhushan, Sarvesh, Umesh");
 });
 
 const PORT = process.env.PORT | 80;
